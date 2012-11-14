d3995f2ce0d817dffd1c0415fad278602ef3a167
Yury Kaliada <fut.wrk@gmail.com>
2012-11-14 17:44:37 +0300

    Delete directories before create
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- vim-dotfiles/setup.sh
+++ vim-dotfiles/setup.sh
@@ -4,9 +4,13 @@ ln -nfs "`pwd`/vimrc" ~/.vimrc
 ln -nfs "`pwd`/gvimrc" ~/.gvimrc
 ln -nfs "`pwd`/vim" ~/.vim
 
+rm -rf ~/.vim/tmp
+rm -rf ~/.vim/backup
+
 mkdir ~/.vim/tmp
 mkdir ~/.vim/backup
 
+rm -rf  ~/.vim/bundle/vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 sudo apt-get install ack-grep python-fontforge
