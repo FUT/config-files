cfa1b0b714e699c0673dcddc0f5caba8e1841301
yury.kaliada <fut.wrk@gmail.com>
2013-01-05 08:33:24 +0300

    Vim compile with python
    Signed-off-by: yury.kaliada <fut.wrk@gmail.com>

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install mercurial
-sudo apt-get install mercurial
+sudo apt-get install mercurial python-setuptools python2.7 libgnome2-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev libperl-dev python-dev libncurses5-dev
 
 # Compile vim from source
 hg clone https://vim.googlecode.com/hg/ vim
@@ -10,10 +10,11 @@ hg update
 
 cd vim/src
 ./configure --with-features=huge \
-  --enable-rubyinterp=yes \
-  --enable-pythoninterp=yes \
+  --enable-rubyinterp \
+  --enable-pythoninterp \
   --enable-cscope \
-  --enable-fontset
+  --enable-fontset \
+  --with-x
 
 make
 sudo make install
