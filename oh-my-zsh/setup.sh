277419fbb113723bd7351edb1e1153b578a7dcc8
Yury Kaliada <fut.wrk@gmail.com>
2013-10-22 13:16:14 +0300

    major cleaning
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- oh-my-zsh/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Remove default git aliases
-rm -vf ~/.oh-my-zsh/plugins/git/git.plugin.zsh
-
-# Replace aliases
-ln -vnfs "`pwd`/aliases.zsh" ~/.oh-my-zsh/lib/aliases.zsh
---
+++ zsh-local.zsh
@@ -0,0 +1,11 @@
+export RUBY_HEAP_SLOTS_INCREMENT=1000000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+export RUBY_GC_MALLOC_LIMIT=100000000
+export RUBY_HEAP_FREE_MIN=500000
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export SYS_NOTIFIER=/usr/local/bin/terminal-notifier
+
+export PATH="/usr/local/bin:/usr/local/sbin:~/bin:$PATH"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
